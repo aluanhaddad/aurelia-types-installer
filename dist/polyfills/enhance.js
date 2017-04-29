9cc4e9c8bdc4493a2b1da44b5b054c97adc5dc86
Aluan Haddad <aluanh@gmail.com>
2017-04-29 07:11:20 -0400

    remove dist

--- dist/polyfills/enhance.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var index_1 = require("./index");
-index_1.default();
-index_1.imbueArray();
-//# sourceMappingURL=enhance.js.map
--- dist/polyfills/object-entries.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function polyfillObjectEntries() {
-    if (typeof Object.entries !== 'function') {
-        Object.entries = function (o) { return Object.keys(o).map(function (key) { return [key, o[key]]; }); };
-    }
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = polyfillObjectEntries;
-//# sourceMappingURL=object-entries.js.map
