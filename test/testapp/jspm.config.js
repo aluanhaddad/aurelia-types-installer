3e840dc857c5610f6a36fc15b63f1e08f189b1da
Aluan Haddad <aluanh@gmail.com>
2017-06-24 19:07:22 -0400

    update test app

--- test/testapp/jspm.config.js
+++ test/testapp/jspm.config.js
@@ -271,32 +271,33 @@ SystemJS.config({
         "npm:*.json"
     ],
     map: {
+        "aurelia-bootstrap": "github:tochoromero/aurelia-bootstrap@0.1.19",
         "babel-plugin-transform-async-to-module-method": "npm:babel-plugin-transform-async-to-module-method@6.8.0",
         "babel-plugin-transform-decorators-legacy": "npm:babel-plugin-transform-decorators-legacy@1.3.4",
         "babel-plugin-transform-flow-strip-types": "npm:babel-plugin-transform-flow-strip-types@6.14.0",
         "babel-runtime": "npm:babel-runtime@6.11.6",
-        "aurelia-binding": "npm:aurelia-binding@1.0.3",
+        "aurelia-binding": "npm:aurelia-binding@1.2.1",
         "aurelia-bootstrapper": "npm:aurelia-bootstrapper@1.0.0",
-        "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0",
+        "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.1",
         "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.0",
         "aurelia-framework": "npm:aurelia-framework@1.0.2",
         "aurelia-history": "npm:aurelia-history@1.0.0",
         "aurelia-history-browser": "npm:aurelia-history-browser@1.0.0",
         "aurelia-loader": "npm:aurelia-loader@1.0.0",
         "aurelia-loader-default": "npm:aurelia-loader-default@1.0.0",
-        "aurelia-logging": "npm:aurelia-logging@1.0.0",
+        "aurelia-logging": "npm:aurelia-logging@1.3.1",
         "aurelia-logging-console": "npm:aurelia-logging-console@1.0.0",
-        "aurelia-metadata": "npm:aurelia-metadata@1.0.0",
-        "aurelia-pal": "npm:aurelia-pal@1.0.0",
+        "aurelia-metadata": "npm:aurelia-metadata@1.0.3",
+        "aurelia-pal": "npm:aurelia-pal@1.3.0",
         "aurelia-pal-browser": "npm:aurelia-pal-browser@1.0.0",
-        "aurelia-path": "npm:aurelia-path@1.0.0",
+        "aurelia-path": "npm:aurelia-path@1.1.1",
         "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0",
         "aurelia-route-recognizer": "npm:aurelia-route-recognizer@1.0.0",
         "aurelia-router": "npm:aurelia-router@1.0.2",
-        "aurelia-task-queue": "npm:aurelia-task-queue@1.0.0",
-        "aurelia-templating": "npm:aurelia-templating@1.0.1",
+        "aurelia-task-queue": "npm:aurelia-task-queue@1.2.0",
+        "aurelia-templating": "npm:aurelia-templating@1.4.2",
         "aurelia-templating-binding": "npm:aurelia-templating-binding@1.0.0",
-        "aurelia-templating-resources": "npm:aurelia-templating-resources@1.0.0",
+        "aurelia-templating-resources": "npm:aurelia-templating-resources@1.4.0",
         "aurelia-templating-router": "npm:aurelia-templating-router@1.0.0",
         "assert": "npm:jspm-nodelibs-assert@0.2.0",
         "buffer": "npm:jspm-nodelibs-buffer@0.2.0",
@@ -308,7 +309,7 @@ SystemJS.config({
         "fs": "npm:jspm-nodelibs-fs@0.2.0",
         "os": "npm:jspm-nodelibs-os@0.2.0",
         "path": "npm:jspm-nodelibs-path@0.2.0",
-        "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+        "process": "npm:jspm-nodelibs-process@0.2.0",
         "stream": "npm:jspm-nodelibs-stream@0.2.0",
         "string_decoder": "npm:jspm-nodelibs-string_decoder@0.2.0",
         "util": "npm:jspm-nodelibs-util@0.2.0",
@@ -641,6 +642,13 @@ SystemJS.config({
         "npm:jspm-nodelibs-os@0.2.0": {
             "map": {
                 "os-browserify": "npm:os-browserify@0.2.1"
+            }
+        },
+        "github:tochoromero/aurelia-bootstrap@0.1.19": {
+            "map": {
+                "velocity-animate": "npm:velocity-animate@1.5.0",
+                "tether": "npm:tether@1.4.0",
+                "aurelia-templating-resources": "npm:aurelia-templating-resources@1.4.0"
             }
         }
     }
