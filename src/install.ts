125a4e679809b472069acb8a5d4353d18f782fd9
Aluan Haddad <aluanh@gmail.com>
2017-10-05 19:23:20 -0400

    Support for multiple jspm.*.js configuration files

--- src/install.ts
+++ src/install.ts
@@ -1,10 +1,11 @@
 ﻿import './polyfills/enhance';
-import {fs} from 'mz';
+import mz = require('mz');
+const {fs} = mz;
 import path = require('path');
 import deepEqual = require('deep-equal');
 import ensureDir from './ensure-dir';
 import downloadDeclaration from './aquire-declaration';
-import extractJspmConfig from './extract-jspm-config';
+import loadJspmConfiguration from './load-jspm-configuration';
 
 export interface InstallOptions {
   projectDir: string;
@@ -16,83 +17,77 @@ export interface InstallOptions {
 export default async function install({projectDir, framework, dest, explicitIndex}: InstallOptions) {
   const baseUrl = await fs.realpath(projectDir);
 
-  let jspmConfigFileName: string;
-  if (await fs.exists(await fs.realpath(baseUrl + '/jspm.config.js'))) {
-    jspmConfigFileName = baseUrl + path.sep + 'jspm.config.js';
-  } else {
-    jspmConfigFileName = baseUrl + path.sep + 'config.js';
-  }
-  const paths = extractJspmConfig(await fs.realpath(jspmConfigFileName), name => name.split('@').length > 1 && !name.match(/aurelia-types-installer/))
-    .filter(item => item.indexOf(`${framework}-`) > -1)
-    .map(x => x.split(`${framework}-`)[1]);
+  for await (const paths of loadJspmConfiguration({baseUrl, framework})) {
 
-  const {successes, failures} = (await Promise.all(paths.map(async path => {
-    try {
-      await ensureDir(baseUrl + '/' + dest);
+    const {successes, failures} = (await Promise.all(paths.map(async path => {
       try {
-        const result = await downloadDeclaration(baseUrl, dest, path, framework);
-        return {message: result, error: undefined};
-      } catch (e) {
-        return {
-          message: `failed to install declarations for '${framework}-${path}' (could not locate declaration in repository or repository)
+        await ensureDir(baseUrl + '/' + dest);
+        try {
+          const result = await downloadDeclaration(baseUrl, dest, path, framework);
+          return {message: result, error: undefined};
+        } catch (e) {
+          return {
+            message: `failed to install declarations for '${framework}-${path}' (could not locate declaration in repository or repository)
         ${failureWorkaroundHint(path)}`, error: e.message
+          };
+        }
+      } catch (x) {
+        return {message: `failed to install declarations for '${framework}-${path} (destination directory, '${dest}', could not be read)'`, error: x.message};
+      }
+    }))).reduce(({successes, failures}, result) => {
+      if (result.error) {
+        return {
+          successes,
+          failures: [...failures, result]
         };
       }
-    } catch (x) {
-      return {message: `failed to install declarations for '${framework}-${path} (destination directory, '${dest}', could not be read)'`, error: x.message};
-    }
-  }))).reduce(({successes, failures}, result) => {
-    if (result.error) {
       return {
-        successes,
-        failures: [...failures, result]
+        successes: [...successes, result.message],
+        failures
+      };
+    }, {
+        successes: [] as string[], failures: [] as {message: string, error: string}[]
+      });
+
+    const generatedTsConfigPath = baseUrl + path.sep + 'tsconfig.paths.json';
+    const tsConfig: TSConfig = require(await fs.realpath(baseUrl + path.sep + 'tsconfig.json'));
+    let generatedTsConfig: TSConfig;
+    if (await fs.exists(generatedTsConfigPath)) {
+      generatedTsConfig = require(generatedTsConfigPath) as TSConfig;
+    } else {
+      generatedTsConfig = {
+        compilerOptions: {
+          baseUrl: '.',
+          paths: {}
+        }
       };
     }
-    return {
-      successes: [...successes, result.message],
-      failures
-    };
-  }, {
-      successes: [] as string[], failures: [] as {message: string, error: string}[]
-    });
-
-  const generatedTsConfigPath = baseUrl + path.sep + 'tsconfig.paths.json';
-  const tsConfig: TSConfig = require(await fs.realpath(baseUrl + path.sep + 'tsconfig.json'));
-  let generatedTsConfig: TSConfig;
-  if (await fs.exists(generatedTsConfigPath)) {
-    generatedTsConfig = require(generatedTsConfigPath) as TSConfig;
-  } else {
-    generatedTsConfig = {
-      compilerOptions: {
+    if (!generatedTsConfig.compilerOptions) {
+      generatedTsConfig.compilerOptions = Object.assign(generatedTsConfig.compilerOptions, {
+        paths: tsConfig.compilerOptions.paths || {},
         baseUrl: '.',
-        paths: {}
-      }
-    };
-  }
-  if (!generatedTsConfig.compilerOptions) {
-    generatedTsConfig.compilerOptions = Object.assign(generatedTsConfig.compilerOptions, {
-      paths: tsConfig.compilerOptions.paths || {},
-      baseUrl: '.',
+      });
+    }
+    const {compilerOptions} = generatedTsConfig;
+
+    paths.forEach(name => {
+      const existingEntries = compilerOptions.paths[`${framework}-${name.split('@')[0]}`];
+      const newEntry = `${dest}/${framework}-${name}${explicitIndex ? '/index' : ''}`;
+      compilerOptions.paths[`${framework}-${name.split('@')[0]}`] = existingEntries ? [...existingEntries.filter(entry => entry !== newEntry), newEntry] : [newEntry];
     });
-  }
-  const {compilerOptions} = generatedTsConfig;
+    if (!explicitIndex && !tsConfig.compilerOptions.moduleResolution && !generatedTsConfig.compilerOptions.moduleResolution) {
+      generatedTsConfig.compilerOptions.moduleResolution = 'node';
+    }
 
-  paths.forEach(name => {
-    const existingEntries = compilerOptions.paths[`${framework}-${name.split('@')[0]}`];
-    const newEntry = `${dest}/${framework}-${name}${explicitIndex ? '/index' : ''}`;
-    compilerOptions.paths[`${framework}-${name.split('@')[0]}`] = existingEntries ? [...existingEntries.filter(entry => entry !== newEntry), newEntry] : [newEntry];
-  });
-  if (!explicitIndex && !tsConfig.compilerOptions.moduleResolution && !generatedTsConfig.compilerOptions.moduleResolution) {
-    generatedTsConfig.compilerOptions.moduleResolution = 'node';
+    if (!deepEqual(generatedTsConfig, tsConfig, {strict: true})) {
+      await fs.writeFile(baseUrl + path.sep + 'tsconfig.paths.json', JSON.stringify(generatedTsConfig, (_, value) => value, 2));
+    }
+    const {successSummary, failureSummary} = buildSummary(successes, failures);
+    return {successSummary, failureSummary};
+
   }
 
-  if (!deepEqual(generatedTsConfig, tsConfig, {strict: true})) {
-    await fs.writeFile(baseUrl + path.sep + 'tsconfig.paths.json', JSON.stringify(generatedTsConfig, (_, value) => value, 2));
-  }
-  const {successSummary, failureSummary} = buildSummary();
-  return {successSummary, failureSummary};
-
-  function buildSummary() {
+  function buildSummary(successes: string[], failures: {message: string}[]) {
     return {
       successSummary: `Installed ${successes.length} ${framework} type declarations:
 
@@ -109,20 +104,21 @@ ${failures.map(({message}) => ` - ${message}`).join(`\n`)}
   function failureWorkaroundHint(dep: string) {
     const name = `${framework}-${dep.split('@')[0] || dep}`;
     return `
-  consider:
-    - adding
-        "${name}": ["jspm_packages/npm/${name}"]
-      to the "paths" object of the "compilerOptions" object of your tsconfig.paths.json
-    - running
-        $ jspm install npm:@types/${name}
-    - running
-        $ npm install --save @types/${name}
-    - adding
-        declare module "${name}";
-      to a global .d.ts file
-    `;
+consider:
+  - adding
+      "${name}": ["jspm_packages/npm/${name}"]
+    to the "paths" object of the "compilerOptions" object of your tsconfig.paths.json
+  - running
+      $ jspm install npm:@types/${name}
+  - running
+      $ npm install --save @types/${name}
+  - adding
+      declare module "${name}";
+    to a global .d.ts file
+  `;
   }
 }
+
 interface TSConfig {
   compilerOptions: {
     moduleResolution?: 'node' | 'classic'
---
+++ src/load-jspm-configuration.ts
@@ -0,0 +1,17 @@
+import mz = require('mz');
+import extractJspmConfig from './extract-jspm-config';
+import path = require('path');
+const {fs} = mz;
+
+export default async function* loadJspmConfiguration({baseUrl, framework}: {baseUrl: string, framework: string}) {
+  for (const filePath of ['config.js', 'jspm.config.js', 'jspm.dev.js', 'jspm.browser.js', 'jspm.node.js']) {
+    const jspmConfigFileName = baseUrl + path.sep + filePath;
+    if (await fs.exists(await fs.realpath(jspmConfigFileName))) {
+
+      const paths = extractJspmConfig(await fs.realpath(jspmConfigFileName), name => name.split('@').length > 1 && !name.match(/aurelia-types-installer/))
+        .filter(item => item.indexOf(`${framework}-`) > -1)
+        .map(x => x.split(`${framework}-`)[1]);
+      yield paths;
+    }
+  }
+}
