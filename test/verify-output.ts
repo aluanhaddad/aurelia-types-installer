cbc19e776ce017480422bc01b8f4978546766bd1
Aluan Haddad <aluanh@gmail.com>
2019-08-01 08:16:18 -0400

    fix typo in test

--- test/verify-output.ts
+++ test/verify-output.ts
@@ -5,13 +5,13 @@ import process from 'process';
 const input = process.argv[2];
 console.info(process.argv);
 
-test('ouput should contain expected successfully retrieved declaration count', async ({ok, plan}) => {
+test('output should contain expected successfully retrieved declaration count', async ({ok, plan}) => {
   plan(1);
   const data = await fs.readFile(input);
   ok(data && String(data).match(/Installed 24 aurelia type declarations/g));
 });
 
-test('ouput should contain expected unsuccessfully retrieved declaration count', async ({ok, plan}) => {
+test('output should contain expected unsuccessfully retrieved declaration count', async ({ok, plan}) => {
   plan(1);
   const data = await fs.readFile(input);
   ok(data && String(data).toString().match(/Unable to locate type declarations for 2 aurelia packages/g));
